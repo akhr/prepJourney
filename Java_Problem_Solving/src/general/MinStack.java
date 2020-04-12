eea0aa98b16dcf4a040fc46c0b41a0cb7eeb541a
Akhash Ramamurthy <akhashr@gmail.com>
2020-04-11 23:52:00 -0700

    MinStack - Stack with min().

---
+++ Java_Problem_Solving/src/general/MinStack.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package general;
+
+import inplace_reverse_linkedlist.ReverseNodeInAlternateKGroup.ListNode;
+
+/**
+ * @author ramamurthy
+ *
+ */
+public class MinStack {
+
+	public class ListNode {
+		int val;
+		ListNode next;
+		int minVal;
+		ListNode(int x) { val = x; }
+	}
+
+	/** initialize your data structure here. */
+	ListNode head;
+	ListNode tail;
+
+	public MinStack() {
+		head = null;
+		tail = null;
+	}
+
+	public void push(int x) {
+		ListNode newNode = new ListNode(x);
+		newNode.next = head;
+		if (head == null || (head != null && head.minVal > x)) {
+			newNode.minVal = x;
+		} else {
+			newNode.minVal = head.minVal;
+		}
+		head = newNode;
+	}
+
+	public void pop() {
+		if (head != null) {
+			ListNode temp = head;
+			head = head.next;
+			temp.next = null;
+		}
+	}
+
+	public int top() {
+		return head.val;
+	}
+
+	public int getMin() {
+		return head.minVal;
+	}
+}
