d4c74ad5a35871afc9106f55c3be10de0dc8ac98
Akhash Ramamurthy <akhashr@gmail.com>
2020-04-14 14:31:20 -0700

    Refactor and different strategy how and when to move left when we encounter a repeating char

--- Java_Problem_Solving/src/slidingWindow/LongestSubstringWithoutRepeatingCharacters_3.java
+++ Java_Problem_Solving/src/slidingWindow/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -28,9 +28,27 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
 		
 		while(right < s.length()) {
 			char c = s.charAt(right);
+			
+			//If contains then we need to exclude the char from current window
 			if (charIndxMap.containsKey(c)) {
+				// Get lastIndex where this char was seen.
+				// Eg a b b a
+				//    0 1 2 3
+				// When 2nd time we see 'a' @ index 3 Map = {{a,0}, {b,2}}, left = 2 and right = 3
+				// since left is at 2 we already excluded from the window. But how will we know?
 				int lastIndexOfThisChar = charIndxMap.get(c);
-				left = Math.max(left, lastIndexOfThisChar + 1);
+				// Either I can check 
+				//  - if window already excluded 'a' by checking lastIndex(a) < left
+				//      - if yes, we moved past 'a'. No action needed.
+				//      - if no, new left = lastIndex(a) + 1
+				if (lastIndexOfThisChar >= left) {
+					left = lastIndexOfThisChar + 1;
+				}
+				
+				// OR
+				
+				//  - Simply Math.max(left, lastIndex(a)+1) 
+				// left = Math.max(left, lastIndexOfThisChar + 1);
 			}
 			charIndxMap.put(c, right);
 			maxLen = Math.max(maxLen, right - left + 1);
