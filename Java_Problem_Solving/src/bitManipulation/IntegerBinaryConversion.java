b902fdedb6e495276328df1a7e9dcd46578a8917
Akhash Ramamurthy <akhashr@gmail.com>
2020-04-16 11:35:43 -0700

    Replace arith ops with bit ops

--- Java_Problem_Solving/src/bitManipulation/IntegerBinaryConversion.java
+++ Java_Problem_Solving/src/bitManipulation/IntegerBinaryConversion.java
@@ -11,18 +11,17 @@ public class IntegerBinaryConversion {
 		while (a >= 1) {
 			int bit = a % 2;
 			sB.append(bit);
-			a /= 2;
+			a = a >> 1;
 		}
 		// Reverse required
 		return sB.reverse().toString();
 	}
-
+	
 	public int binaryToInt(String b) {
 		char[] charArr = b.toCharArray();
 		int dec = 0;
 		for(int i=charArr.length-1, exp=0; i>=0; i--, exp++) {
-			int t =  (int)Math.pow(2, exp);
-			dec += (charArr[i]-48) * t;
+			dec += (charArr[i]-48) * (1 << exp);
 		}
 		return dec;
 	}
@@ -50,5 +49,38 @@ public class IntegerBinaryConversion {
 		int actual = binaryToInt(b);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void test_04() {
+		String b = "10001";
+		int expected = 17;
+		int actual = binaryToInt(b);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_05() {
+		String b = "01111110";
+		int expected = 126;
+		int actual = binaryToInt(b);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_06() {
+		String b = "0000000110000011";
+		int expected = 387;
+		int actual = binaryToInt(b);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_07() {
+		int a = 387;
+		String expected = "110000011";
+		String actual = intToBinary(a);
+		assertEquals(expected, actual);
+	}
+
 
 }
