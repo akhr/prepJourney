2891575441c9712c4c406509de9fceacf9eafeb9
Akhash Ramamurthy <akhashr@gmail.com>
2020-04-18 22:54:49 -0700

    Frequency sort of a string implemented using heap.

---
+++ Java_Problem_Solving/src/heap/SortCharactersByFrequency_451.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package heap;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import org.junit.Test;
+
+/**
+ * @author ramamurthy
+ *
+ */
+public class SortCharactersByFrequency_451 {
+	public String frequencySort(String s) {
+		Map<Character, Integer> freqMap = new HashMap<>();
+		for(char c : s.toCharArray()) {
+			freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+		}
+		
+		Queue<Character> q = new PriorityQueue<>(s.length(), new Comparator<Character>() {
+			@Override
+			public int compare(Character c1, Character c2) {
+				return freqMap.get(c2) - freqMap.get(c1);
+			}
+		});
+		
+		for(Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+			q.add(entry.getKey());
+		}
+		
+		StringBuilder sB = new StringBuilder(s.length());
+		while(!q.isEmpty()) {
+			char c = q.poll();
+			for(int i=0; i<freqMap.get(c); i++) {
+				sB.append(c);
+			}
+		}
+		return sB.toString();
+	}	
+	
+	@Test
+	public void test_01() {
+		String s = "tree";
+		String expected = "eert";
+		assertEquals(expected, frequencySort(s));
+	}
+	
+	@Test
+	public void test_02() {
+		String s = "cccaaa";
+		String expected = "aaaccc";
+		assertEquals(expected, frequencySort(s));
+	}
+	
+	@Test
+	public void test_03() {
+		String s = "Aabb";
+		String expected = "bbAa";
+		assertEquals(expected, frequencySort(s));
+	}
+}
