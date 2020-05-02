1920d5fb34f631a5f6893cec214c223337151f2a
Akhash Ramamurthy <akhashr@gmail.com>
2020-05-02 06:53:18 -0700

    DFS level order traversal.

--- Java_Problem_Solving/src/bfs/BinaryTreeLevelOrderTraversal_102.java
+++ Java_Problem_Solving/src/bfs/BinaryTreeLevelOrderTraversal_102.java
@@ -25,10 +25,9 @@ public class BinaryTreeLevelOrderTraversal_102 {
 	}
 
 	public static List<List<Integer>> traverse(Node root){
-		if (root == null) {
-			return new ArrayList<>();
-		}
 		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) return res;
+		
 		Queue<Node> q = new LinkedList<>();
 		q.add(root);
 		int levelSize;
@@ -47,6 +46,24 @@ public class BinaryTreeLevelOrderTraversal_102 {
 		return res;
 	}
 	
+	public static List<List<Integer>> traverse_DFS(Node root){
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) return res;
+		helper(root, 1, res);
+		return res;
+	}
+	
+	private static void helper(Node root, int level, List<List<Integer>> res) {
+		if (root == null) return;
+		if(res.size() == level-1) { //still in prev level
+			res.add(new ArrayList<>());
+		}
+		//visit the node
+		res.get(level-1).add(root.val); // arr index starts at 0
+		helper(root.left, level+1, res);
+		helper(root.right, level+1, res);
+	}
+	
 	@Test
 	public void test_01() {
 		Node root = new Node(12);
@@ -56,7 +73,19 @@ public class BinaryTreeLevelOrderTraversal_102 {
 	    root.right.left = new Node(10);
 	    root.right.right = new Node(5);
 	    List<List<Integer>> result = BinaryTreeLevelOrderTraversal_102.traverse(root);
-	    System.out.println("Level order traversal: " + result);
+	    System.out.println("BFS Level order traversal: " + result);
+	}
+	
+	@Test
+	public void test_02() {
+		Node root = new Node(12);
+	    root.left = new Node(7);
+	    root.right = new Node(1);
+	    root.left.left = new Node(9);
+	    root.right.left = new Node(10);
+	    root.right.right = new Node(5);
+	    List<List<Integer>> result = BinaryTreeLevelOrderTraversal_102.traverse_DFS(root);
+	    System.out.println("DFS Level order traversal: " + result);
 	}
 
 }
